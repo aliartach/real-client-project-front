7dc4c3bccce404f619b1f11fde18e722ad83703d
alikansoh <alikansoh.dev@gmail.com>
2024-01-12 17:23:07 +0200

    vite installed

---
+++ Bone guy/src/Components/header.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function header() {
+  return (
+    <div className='Header-Home'>
+        <section className='top-'> 
+
+
+        </section>
+    </div>
+  )
+}
+
+export default header
---
+++ Bone guy/src/Pages/Home.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import Header from '../Components/header'
+function Home() {
+  return (
+    <div>
+        <Header/>
+    </div>
+  )
+}
+
+export default Home

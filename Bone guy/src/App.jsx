16bf6b8ed8a3b10bf22ae5f5a0c9975a527d37fc
zahraaAed <zahraaalaaeddine94@gmail.com>
2024-01-22 16:59:25 +0200

    add navbar

--- Bone guy/src/App.jsx
+++ Bone guy/src/App.jsx
@@ -1,14 +1,14 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import OurStory from './Components/ourStory'; // Adjust the path accordingly
-
+import AdminDashboard from './Pages/AdminDashboard/AdminDashboard';
 function App() {
   return (
     <>
       <Router>
         <Routes>
-          <Route path="/story" element={<OurStory storyText="Your story text goes here" />} />
-          {/* Adjust the storyText prop with the actual story text you want to pass */}
+          <Route path="/story" element={<OurStory/>} />
+          <Route path="/admin/dashboard" element={<AdminDashboard/>} />
         </Routes>
       </Router>
     </>

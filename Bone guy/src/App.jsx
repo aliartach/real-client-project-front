767b7a169cb7a6e3f1c4f2b9f1338182caf64fe3
Hassan Mourtada <hassanmourtada15@gmail.com>
2024-01-29 11:20:25 +0200

    some changes 02

--- Bone guy/src/App.jsx
+++ Bone guy/src/App.jsx
@@ -1,17 +1,18 @@
 import './App.css'
 import Home from './Pages/Home'
-import AboutUs from './Pages/AboutUs'
-import FAQ from './Pages/FAQs'
+import AboutUs from './Pages/AboutUs/AboutUs'
+import FAQ from './Pages/FAQs/FAQs'
 import Policy from './Pages/policy/policy'
 import ContactUs from './Pages/Contactus/ContactUs'
-// import AboutUs from './Pages/AboutUs'
-// import FAQ from './Pages/FAQs'
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 // import Featured from './Components/Featured-section/featured-products/featured-products'
-// import SubCategories from './Components/SubCategories/SubCategories'
 import AdminDashboard from "./Pages/AdminDashboard/AdminDashboard";
 import AdminContent from "./Pages/Content-Admin/content";
 import AdminSubCategories from "./subCategories-Admin/subCategories";
+import Login from './Components/Login/Login'
+import SignUp from './Components/SignUp/SignUp'
+
+
 function App() {
   return (
     <Router>
@@ -19,23 +20,19 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/aboutus" element={<AboutUs />} />
         {/* <Route path="/product" element={<Product />} /> */}
-
         <Route path="/" element={<FAQ />} />
         <Route path="/Policy" element={<Policy />} />
         <Route path="/ContactUs" element={<ContactUs />} />
+        <Route path="/" element={<AboutUs />} />
+        <Route path="/FAQs" element={<FAQ />} />
+        <Route path='/admin' element={<AdminDashboard />}>
+          {/* <Route path='/products' element={<AdminProducts/>}/> */}
+          <Route path='admin/content/' element={<AdminContent />} />
+          <Route path='admin/subCategories/' element={<AdminSubCategories />} />
+        </Route>
+        <Route path='/login' element={<Login />} />
+        <Route path='/signup' element={<SignUp />} />
 
-
-        {/* <Route path="/" element={<SubCategories />} /> */}
-        {/* <Route path="/" element={<AboutUs />} /> */}
-        {/* <Route path="/" element={<FAQ />} /> */}
-     
-      <Route path='/admin' element={<AdminDashboard/>}>
-        {/* <Route path='/products' element={<AdminProducts/>}/> */}
-        <Route path='admin/content/' element={<AdminContent/>}/>
-        <Route path='admin/subCategories/' element={<AdminSubCategories/>}/>
-
-      </Route>
- 
       </Routes>
     </Router>
   );

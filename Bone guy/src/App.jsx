30dcee62bc66a70aae309291937fbe2de5f2d971
Kamal <kamal_dia@hotmail.com>
2024-01-31 19:13:21 +0200

    merged with main

--- Bone guy/src/App.jsx
+++ Bone guy/src/App.jsx
@@ -7,39 +7,38 @@ import FAQ from './Pages/FAQs/FAQs'
 import Policy from './Pages/policy/policy'
 import ContactUs from './Pages/Contactus/ContactUs'
 // import Featured from './Components/Featured-section/featured-products/featured-products'
-import AdminProducts from './Pages/Admin/Admin-products/admin-products.jsx'
-import AdminDashboard from "./Pages/Admin/AdminDashboard/AdminDashboard.jsx";
-import AdminContent from "./Pages/Admin/Content-Admin/content.jsx";
-import AdminSubCategories from "./Pages/Admin/subCategories-Admin/subCategories.jsx";
-import Login from './Components/Login/Login.jsx'
-import SignUp from './Components/SignUp/SignUp.jsx'
+import AdminDashboard from "./Pages/AdminDashboard/AdminDashboard";
+import AdminContent from "./Pages/Content-Admin/content";
+import AdminSubCategories from "./subCategories-Admin/subCategories";
+import Login from './Components/Login/Login'
+import SignUp from './Components/SignUp/SignUp'
 import Product from './Pages/Product/Product';
-
-
 
 function App() {
 
   return (
-    <AdminProducts/>
-    // <Router>
-    //   <Routes>
-    //     <Route path="/" element={<Home />} />
-    //     <Route path="/aboutus" element={<AboutUs />} />
-    //     {/* <Route path="/product" element={<Product />} /> */}
-    //     <Route path="/" element={<FAQ />} />
-    //     <Route path="/Policy" element={<Policy />} />
-    //     <Route path="/ContactUs" element={<ContactUs />} />
-    //     <Route path="/" element={<AboutUs />} />
-    //     <Route path="/FAQs" element={<FAQ />} />
-    //     <Route path='/admin' element={<AdminDashboard />}>
-    //       <Route path='/products' element={<AdminProducts/>}/>
-    //       <Route path='admin/content/' element={<AdminContent />} />
-    //       <Route path='admin/subCategories/' element={<AdminSubCategories />} />
-    //     </Route>
-    //     <Route path='/login' element={<Login />} />
-    //     <Route path='/signup' element={<SignUp />} />
-    //   </Routes>
-    // </Router>
+    <Router>
+
+      {/* <NavBarPolicies /> */}
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/aboutus" element={<AboutUs />} />
+        <Route path="/product" element={<Product />} />
+        <Route path="/" element={<FAQ />} />
+        <Route path="/Policy" element={<Policy />} />
+        <Route path="/ContactUs" element={<ContactUs />} />
+        <Route path="/" element={<AboutUs />} />
+        <Route path="/FAQs" element={<FAQ />} />
+        <Route path='/admin' element={<AdminDashboard />}>
+          {/* <Route path='/products' element={<AdminProducts/>}/> */}
+          <Route path='admin/content/' element={<AdminContent />} />
+          <Route path='admin/subCategories/' element={<AdminSubCategories />} />
+        </Route>
+        <Route path='/login' element={<Login />} />
+        <Route path='/signup' element={<SignUp />} />
+
+      </Routes>
+    </Router>
   );
 }
 

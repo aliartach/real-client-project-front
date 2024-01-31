d9b635c167733ec2277c222d00df786224103280
Kamal <kamal_dia@hotmail.com>
2024-01-31 18:54:10 +0200

    finished admin product functionalities

---
+++ Bone guy/src/Components/Admin-content-card/Admin-product-card.jsx
@@ -0,0 +1,40 @@
+import React, {useState} from "react";
+import Editproduct from "../../Pages/Admin/Admin-products/Edit-product";
+import "./Admin-product-card.css";
+
+const ProductCard = ({product, tags, sub_categories, handleProductDelete, fetchAdminProducts}) => {
+  const [product_edit_status, setProductEditStatus] = useState(false);
+  //handleProductDelete is in admin-products
+
+  return(
+  <div className="container-of-product-card-and-edit-product-in-admin-products">
+    <article className="product-card">
+      <img src={`http://localhost:4000/${product.image}`} alt="product image" className="product-image-in-product-card"/>
+      <section className="product-details-in-product-card">
+        <h2 className="product-name-in-product-card">Name: {product.name}</h2>
+        <p className="product-description-in-product-card">Description: {product.description}</p>
+        <p className="product-price-in-product-card">Price: {product.price}</p>
+        <p className="product-quantity-in-product-card">Quantity: {product.quantity}</p>
+        <p className="product-weight-in-product-card">Weight: {product.weight} g</p>
+        <p className="product-category-in-product-card">Category: {product.category} </p>
+        {product.sub_categories.length >0 && <p className="product-subcategories-label-in-product-card">Sub Categories:</p>}
+        {product.sub_categories.length > 0 && product.sub_categories.map((sub_category, index) => {
+          return <p key={index} className="product-single-subcategory-in-product-card">{sub_category.name}</p>
+        })}
+        {product.tags.length > 0 && <p className="product-tags-label-in-product-card">Tags:</p>}
+        {product.tags.length > 0 && product.tags.map((tag, index) => {
+          return <p key={index} className="product-single-tag-in-product-card">{tag.name}</p>
+        })}
+        {product.featured && <p className="product-featured-in-product-card">Featured</p>}
+      </section>
+      <section className="product-buttons-in-product-card">
+        <button type="button" className="edit-product-button-in-product-card" onClick={() => setProductEditStatus(true)}>Edit</button>
+        <button type="button" className="delete-product-button-in-product-card" onClick={() => handleProductDelete(product)}>Delete</button>
+      </section>
+    </article>
+    {product_edit_status && <Editproduct tags={tags} sub_categories={sub_categories} setProductEditStatus={setProductEditStatus} product={product} fetchAdminProducts={fetchAdminProducts} />}
+  </div>
+  )
+}
+
+export default ProductCard;
